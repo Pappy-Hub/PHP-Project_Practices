94446eb5072b2add442b671b361e096c5b9960e9
Pappy <65431875+Pappy-Hub@users.noreply.github.com>
2022-05-17 02:01:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>PHP Browser Test</title>
   </head>
   <body>
-    <h1>I am Back</h1>
-    
     <?php
-echo "<h2>Hello world\nThis is my world\nThis is my world</h2>";
-?>
+     echo "<h1>The Rule Is Back</h1>"; 
+    <?
+      
+    <?php
+     echo "<h2>Hello world\nThis is my world\nThis is my world</h2>";
+    
+    ?>
   </body>
 </html>
